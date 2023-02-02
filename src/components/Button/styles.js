ad6a6bd113811dee9bc68ffcab6ec3d169d5dc30
Student <student@Mac-28310.local>
2023-02-02 15:49:59 +0200

    Create Splash screen

---
+++ src/components/Button/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+   container: {
+    backgroundColor: "blue",
+    paddingVertical: 20,
+    paddingHorizontal: 8,
+    borderRadius: 10,
+    width: "100%"
+
+   },
+   title: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: 'center'
+},
+});
