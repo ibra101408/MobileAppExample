153e23cd51f5c78ecf81128f61810e768fca2ac5
Student <student@Mac-28310.local>
2023-02-10 12:26:03 +0200

    sign in

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native";
 //import Splash from './src/screens/auth/Splash'
 import Signup from "./src/screens/auth/Signup";
+import Signin from "./src/screens/auth/Signin";
 
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import Config from "react-native-config";
@@ -19,7 +20,7 @@ const App = () => {
     }, [])
     return(
         <SafeAreaView>
-            <Signup />
+            <Signin />
         </SafeAreaView>
     );
 };
